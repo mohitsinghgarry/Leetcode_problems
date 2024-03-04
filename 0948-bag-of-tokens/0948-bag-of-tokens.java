27984fba24f716bdfc45c6a20f1aff3b8df157e9
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-03-04 15:01:19 +0530

    Time: 2 ms (97.13%), Space: 42.4 MB (85.03%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,25 +1,23 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        int len = tokens.length;
-        int score = 0;
         int i = 0;
-        int j = len - 1;
+        int j = tokens.length-1;
+        int score  = 0;
         Arrays.sort(tokens);
-        
-        while(i <= j){
-            if(tokens[i] <= power){
-                power -= tokens[i];
-                score++;
-                i++;
-            }else if(score > 0 && i != j){
-                power += tokens[j];
-                score--;
-                j--;
-            }else{
-                break;
+        while(i<=j){
+            if(power>=tokens[i]){
+              power-=tokens[i];
+              i++;
+              score++;
             }
+            else if(score>0 && i!=j){
+                power+=tokens[j];
+                j--;
+                score--;
+            }
+            else break;
         }
-        
         return score;
     }
+
 }
