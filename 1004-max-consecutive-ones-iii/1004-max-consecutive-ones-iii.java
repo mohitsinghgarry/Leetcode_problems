dcafbef20d2e3144a4249ea3976d8647533fa9c2
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-06-08 17:08:38 +0530

    Time: 3 ms (83.83%), Space: 46.9 MB (61.45%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,8 +3,9 @@ class Solution {
         int l = 0, r = 0, max = 0,count = 0,len = 0;
         while(r<nums.length){
             if(nums[r] == 0) count++;
-            while(count > k ){
+            if(count > k ){
                 if(nums[l]==0) count--;
+                // l++;
                 l++;
             }
             if(count<=k){
