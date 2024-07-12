457fbb8f410d50292eaaefc1c2501af35faa5a39
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-07-13 00:05:41 +0530

    Time: 3 ms (53.6%), Space: 45.6 MB (44.42%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1) return true;
-        if(nums.length == 2 && nums[0]<1) return false;
-         if( nums.length>2 && nums[nums[0]] ==0 && nums[0]==nums.length) return false;
-         return true;
+        int max = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(i>max) return false;
+            max = Math.max(max ,i+nums[i]);
+        }
+      return true;
     }
    
 }
