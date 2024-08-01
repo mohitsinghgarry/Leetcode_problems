0c2b16952c9ce0d7f8507c662d4b72cae3c72d9e
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-08-01 14:06:36 +0530

    Time: 1 ms (82.64%), Space: 42.2 MB (77.37%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,9 +3,10 @@ class Solution {
         // OPTIMSED CODE
         int count = 0;
         for(int i = 0;i<details.length;i++){
-            String str = details[i].substring(11 , 13);
-            int num = Integer.parseInt(str);
-            // System.out.println(num);
+            // String str = details[i].substring(11 , 13);
+            int num1 =  details[i].charAt(11) -'0';
+            int num2 = details[i].charAt(12) - '0';
+            int num = num1*10 + num2;
             if(num > 60) count++;
         }
         return count;
