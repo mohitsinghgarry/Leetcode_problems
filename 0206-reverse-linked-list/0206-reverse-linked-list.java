2d283b769758d5801967cb91f1d1b0a6c1908c1e
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-09-11 19:31:21 +0530

    Time: 0 ms (100%), Space: 42.3 MB (69.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,24 +10,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // BRUTE FORCE APPROACH USING ARRAYLIST
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-       Collections.sort(arr, Collections.reverseOrder());
-            ListNode temp_new = new ListNode();
-        if(arr.size()!= 0) temp_new.val = arr.get(0);
-        else return null;
-        ListNode temp_2 = temp_new;
-        for(int i =1 ;i<arr.size();i++){
-                ListNode temp_final = new ListNode(arr.get(i));
-                temp_new.next = temp_final;
-                temp_new = temp_final;
-        }
-        head = temp_2;
-        return head;
+        // USING 3 POINTERS APPROACH
+        if(head == null) return null;
+       ListNode prev = null;
+       ListNode curr = head;
+       ListNode front = head;
+       while(curr!=null){
+        front = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = front;
+
+       }
+       return prev;
+
+        
     }
 }
