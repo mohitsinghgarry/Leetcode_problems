af8bc476a0fb5a9c3c6f0db3e12cf9b86708e753
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-09-13 18:22:12 +0530

    Time: 2 ms (100%), Space: 55.1 MB (95.96%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
+        // prefix sum approach
         int[] res=  new int[queries.length];
         int xor = 0;
-        for(int i = 0;i<queries.length;i++){
-            int start = queries[i][0];
-            int end = queries[i][1];
-            for(int j = start ;j<=end;j++) xor = xor^arr[j];
-            res[i] = xor;
-            xor = 0;
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = xor^arr[i];
+            xor = arr[i];
         }
-        return res;
+       for(int i = 0;i<queries.length;i++){
+           int start= queries[i][0];
+           int end = queries[i][1];
+           if(start == 0) res[i] = arr[end];
+           else res[i] = arr[end] ^ arr[start-1];
+       }
+       return res;
+       
     }
 }
