6985c09ff59547d91f33a2934b2defae71ab2eb3
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-09-16 16:31:38 +0530

    Time: 18 ms (13.84%), Space: 46.3 MB (21.89%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        // BRUTE FORCE APPROACH
+        int size = timePoints.size()-1;
+        Collections.sort(timePoints);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<timePoints.size()-1;i++){
+        String time2 = timePoints.get(i+1);
+        String time1 = timePoints.get(i);
+        int time_1 =Integer.parseInt(time1.substring(0,2)) * 60 + Integer.parseInt(time1.substring(3));
+        int time_2 =Integer.parseInt(time2.substring(0,2)) * 60 + Integer.parseInt(time2.substring(3));
+         int time = time_2 - time_1;
+        int time_new = 1440 - time;
+        if(time < time_new){
+            min = Math.min(time , min);
+        }
+        else{
+            min = Math.min(time_new , min);
+        }
+        }
+         int time_1 =Integer.parseInt(timePoints.get(0).substring(0,2)) * 60 + Integer.parseInt(timePoints.get(0).substring(3));
+        int time_2 =Integer.parseInt(timePoints.get(size).substring(0,2)) * 60 + Integer.parseInt(timePoints.get(size).substring(3));
+        int time = time_2 - time_1;
+        int time_new = 1440 - time;
+       if(time < time_new){
+            min = Math.min(time , min);
+        }
+        else{
+            min = Math.min(time_new , min);
+        }
+        return min;
+    }
+}
