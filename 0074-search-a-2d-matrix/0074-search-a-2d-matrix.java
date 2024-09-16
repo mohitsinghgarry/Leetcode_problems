f0a9eddd230daa1ce9bb95bb347da35a8aa04fc7
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2024-09-16 19:16:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (27.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,10 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         // BRUTE FORCE APPROACH
-         for(int i = 0;i<matrix.length;i++){
-            for(int j = 0 ;j< matrix[i].length ;j++) {
-                if(matrix[i][j] == target) return true;
-            }
+        boolean n = BinarySearch(matrix,target);
+        return n;
+    }
+      public boolean BinarySearch(int[][] arr, int target){
+         int row = 0;
+         int column = arr[0].length-1;
+         while(row<arr.length && column>=0){
+             if(target == arr[row][column]) return true;
+             else if(target> arr[row][column])row++;
+             else column--;
          }
          return false;
     }
