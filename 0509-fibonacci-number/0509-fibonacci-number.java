d69678019afcad4a8b1fa63cfea1a84d11e082a6
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2025-01-24 21:11:33 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int fib(int n) {
         // RECURRENCE REPLATION OF FIBONACCI ->  f(n-1) + f(n-2)
-        //BRUTE FORCE ONLY RECURSION APPLIED
-        return fibonacci(n) ;
+        //OPTIMISED USING DP (MEMOIZATION)
+        int[] dp = new int[n+1];
+        Arrays.fill(dp , -1);
+        return fibonacci(n, dp) ;
     }
-    public int fibonacci(int n){
+    public int fibonacci(int n ,int [] dp){
           if(n <= 1) return n;
-        return fibonacci(n-1) + fibonacci(n-2);
+          if(dp[n] != -1) return dp[n];
+        return dp[n] = fibonacci(n-1 , dp) + fibonacci(n-2, dp);
     }
 }
