82ecafc8c6fe394032b1ebbb8f93c14f0be6955c
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2025-01-24 21:17:33 +0530

    Time: 0 ms (100%), Space: 40.3 MB (78.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int fib(int n) {
         // RECURRENCE REPLATION OF FIBONACCI ->  f(n-1) + f(n-2)
         //OPTIMISED USING DP (MEMOIZATION)
-        int[] dp = new int[n+1];
+        int[] dp = new int[n+1]; //USED FOR MEMOIZATION ( SIZE = N+1 BECAUSE THERE ARE N+1 CASES FROM 0 TO N)
         Arrays.fill(dp , -1);
         return fibonacci(n, dp) ;
     }
@@ -10,5 +10,9 @@ class Solution {
           if(n <= 1) return n;
           if(dp[n] != -1) return dp[n];
         return dp[n] = fibonacci(n-1 , dp) + fibonacci(n-2, dp);
+
+
+      // TIME COMPLEXITY  = O(N)
+      //SPACE COMPLEXITY = O(N) STACK SPACE + O(N) ARRAY SPACE
     }
 }
