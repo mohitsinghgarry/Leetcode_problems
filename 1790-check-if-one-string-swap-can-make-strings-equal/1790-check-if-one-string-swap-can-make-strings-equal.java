c5c36d01fc572662712856b1fe32014ed6d174aa
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2025-01-28 14:44:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        //CREATING HASHMAP FOR STORING THE KEY VALUE PAIR
+        int count = 0;
+        HashMap<Character , Character> map = new HashMap<>();
+        for(int i = 0;i<s1.length();i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                 map.put(s1.charAt(i) , s2.charAt(i));
+                 count++;
+            }
+        }
+         for (Character key : map.keySet()) {
+              if(!key.equals(map.get(map.get(key)))) return false;
+        }
+        return true;
+        
+        
+    }
+}
