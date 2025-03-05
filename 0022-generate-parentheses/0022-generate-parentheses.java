ba2f551923b919696eef972bfb4e136b254f527b
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2025-03-05 13:40:57 +0530

    Time: 2 ms (50%), Space: 43.4 MB (36.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,34 +1,23 @@
 class Solution {
-    //BRUTE FORCE APPROACH
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        generate(res , n, "");
+        List<String> res = new ArrayList<String>();
+        recurse(res, 0, 0, "", n);
         return res;
     }
-    public void generate(List<String> res , int n, String str){
-        //BASE CASE
-        if(str.length() == 2*n){
-            if(!res.contains(str)) {
-                if(validate(str)) res.add(str);
-            }
-            // System.out.println(str);
+    
+    public void recurse(List<String> res, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
             return;
         }
-        //BACKTRACKING
-        str += '(';
-        generate(res, n , str);
-        str = str.substring(0 , str.length()-1);
-        str += ')';
-        generate(res , n , str);
-    }
-    public boolean validate(String str){
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0;i<str.length();i++){
-            if(str.charAt(i) == '(') stack.push(str.charAt(i));
-            else if(str.charAt(i) == ')' && !stack.isEmpty()) stack.pop();
-            else if(str.charAt(i) == ')' && stack.isEmpty()) return false;
-        }
-        return (stack.isEmpty()) ? true : false;
         
+        if (left < n) {
+            recurse(res, left + 1, right, s + "(", n);
+        }
+        
+        if (right < left) {
+            recurse(res, left, right + 1, s + ")", n);
+        }
     }
+
 }
