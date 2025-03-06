e1f878047e976c028a792aad83827d27d01d952a
Mohit singh <117971044+mohitsinghgarry@users.noreply.github.com>
2025-03-06 22:10:07 +0530

    Time: 148 ms (52.91%), Space: 41.5 MB (77.06%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,30 +1,42 @@
-class Solution {
+public class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i = 0;i<board.length;i++){
-            for(int j = 0;j<board[i].length;j++){
-                if(board[i][j] == word.charAt(0)){
-                   if(existing(word , board, i , j,0)) return true;
+        int m = board.length;
+        int n = board[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
                 }
             }
         }
+        
         return false;
     }
-    public boolean existing(String word , char[][] board , int row , int col, int index){
-        //BASE CASE
-       if (index == word.length()) return true; 
-        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) {
+    
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
-        char temp = board[row][col];
-        board[row][col] = '0';
-        //DFS
-      boolean found = existing(word , board , row  , col+1, index+1)||
-        existing(word , board , row +1 , col, index+1)||
-        existing(word , board , row -1 , col, index+1)||
-        existing(word , board , row , col -1, index+1);
-        //BACKTRACK
-         board[row][col] = temp;
-
-        return found;
+        
+        visited[i][j] = true;
+        
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
+        }
+        
+        visited[i][j] = false;
+        return false;
     }
 }
